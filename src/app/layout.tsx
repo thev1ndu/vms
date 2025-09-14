032f6fabf7de0d5dd014325594451e3e0e7ea33c
Thevindu Wickramaarachchi <thevinduw@MacBook-Air.local>
2025-09-15 00:11:54 +0530

    fix profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
       <body className="min-h-dvh bg-background text-foreground px-[5vh]">
         <DailyPing />
         <BadgeFixer />
-        <div className="max-w-6xl mx-auto p-4">{children}</div>
+        <div className="max-w-6xl mx-auto p-2">{children}</div>
       </body>
     </html>
   );
